035f6e57c441a483c073ea85510a41abc5c06669
sodanhama <brainosoham@gmail.com>
2026-03-11 10:05:51 +0530

    fixed camera, fixed character orientation, added level ground

--- player.gd
+++ player.gd
@@ -5,8 +5,16 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 10
 
 
+var xform : Transform3D	
+
+
 func _physics_process(delta: float) -> void:
-	
+	#Rotate the camera left or right
+	if Input.is_action_just_pressed('cam_left'):
+		$Camera_Controller.rotate_y(deg_to_rad(-30))
+		
+	elif Input.is_action_just_pressed('cam_right'):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,7 +27,25 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+
+	#Move camera	
+	var direction : Vector3 = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	#Rotate the character mesh towards the opposite direction of camera
+	if input_dir != Vector2(0,0):
+		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) -90
+	
+	# Rotate the character in order for it to be aligned with the floor
+	if is_on_floor():
+		align_with_floor($RayCast3D.get_collision_normal())
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+	elif not is_on_floor():
+		align_with_floor(Vector3.UP)
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+			
+	
+	# update the velocity and move the character
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -30,3 +56,11 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	#Mak	e camera controller match the position of myself
 	$Camera_Controller.position = lerp($Camera_Controller.position,position,0.1)
+	
+func align_with_floor(floor_normal):
+	xform = global_transform	
+	xform.basis.y = floor_normal
+	xform.basis.x = -xform.basis.z.cross(floor_normal)
+	xform.basis = xform.basis.orthonormalized()
+	
+	
